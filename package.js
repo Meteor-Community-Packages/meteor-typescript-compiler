e59cff30e7cedd02839ff63085aef52e0946004e
Per Bergland <per@refapp.se>
2024-02-25 16:06:15 +0100

    meteor 3 beta 4

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 /**
  * Match these with the versions in the meteor-typescript/package.js
  */
-const COMPILER_VERSION = "0.4.0-meteor3-beta0";
+const METEOR3_BETA_VERSION = "-beta.4";
+const COMPILER_VERSION = `0.4.0-meteor3${METEOR3_BETA_VERSION}`;
 const TYPESCRIPT_VERSION = "5.3.2";
 
 Package.describe({
@@ -19,9 +20,13 @@ Npm.depends({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom(["2.12", "3.0-beta.0"]);
+  api.versionsFrom(["2.12", `3.0${METEOR3_BETA_VERSION}`]);
   api.use(["babel-compiler"], "server");
-  api.use(["typescript@3.0.0||4.0.0"], "server"); // For compiling this package
+  // For compiling this package.
+  // Typescript itself does not use semver so the meteor package should *not* follow typescript’s version
+  // but I’ve been unable to persuade the core meteor team not to unnecessarily break code that works...
+  // Add a few years of updates here until typescript 8.x appears :)
+  api.use(["typescript@3.0.0||4.0.0||5.0.0||6.0.0||7.0.0"], "server");
   api.addFiles(["meteor-typescript-compiler.ts"], "server");
   api.export(["MeteorTypescriptCompiler"], "server");
 });
